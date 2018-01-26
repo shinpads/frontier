cc752d1f35a3572921a9cf937eeb4dce702bdf25
marshmelllo <robfarlow@gmail.com>
2018-01-25 23:08:59 -0500

    added gun

--- Assets/Scripts/GUI/PlayerGUI.cs
+++ Assets/Scripts/GUI/PlayerGUI.cs
@@ -3,14 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerGUI : MonoBehaviour {
+	[SerializeField] Texture crosshair;
+	void OnGUI() {
+		GUI.Label(new Rect(Screen.width/2 - 16f,Screen.height/2 - 16f, 32f,32f),crosshair);
+	}
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
---
+++ Assets/Scripts/Player/Shooting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+	Camera playerCamera;
+	float shootTime = 10f;
+	[SerializeField] GameObject bullet;
+
+	void Start () {
+		playerCamera = gameObject.GetComponent<PlayerController>().playerCamera;
+	}	
+
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+		Debug.Log("Shoot");
+			GameObject shot = (GameObject) Network.Instantiate (bullet, playerCamera.transform.position + playerCamera.transform.forward, Quaternion.identity, 0);
+			shot.GetComponent<Rigidbody>().AddForce(playerCamera.transform.forward*1000);
+		}
+	}
+}
