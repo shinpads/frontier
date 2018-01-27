29ad640cf54edbb3a453562ad95e2bcf3224eebd
marshmelllo <robfarlow@gmail.com>
2018-01-26 20:54:12 -0500

    fixes to bullet

---
+++ Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	private const int LIFE_SPAN = 3;
+
+	void Start () {
+		StartCoroutine(setTimeOutDestroy());
+	}
+
+	void OnCollisionEnter (Collision collision) {
+	if (!Network.isServer){ return; }
+	if (collision.gameObject.name == "Player(Clone)"){
+			Debug.Log("You hit a heck boi");
+	}
+	Network.Destroy(gameObject);
+	}
+
+	private IEnumerator setTimeOutDestroy () {
+		yield return new WaitForSeconds(LIFE_SPAN);
+		Network.Destroy(gameObject);
+	}
+
+}
