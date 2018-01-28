406dce87c0ac8c71ec1f34e9bd8cb6a0669adba1
marshmelllo <robfarlow@gmail.com>
2018-01-28 16:48:10 -0500

    Fixed player shooting

--- Assets/Scripts/Player/Bullet.cs
+++ Assets/Scripts/Player/Bullet.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour {
 	private const int LIFE_SPAN = 3;
-
+	Vector3 currentPosition, lastPosition;
+	float positionDifference;
+	RaycastHit hit;
+	Rigidbody rigidbod;
 	void Start () {
+		if(!Network.isServer) { enabled = false; }
+		currentPosition = gameObject.transform.position;
+		lastPosition = gameObject.transform.position;
+		rigidbod = gameObject.GetComponent<Rigidbody>();
+		rigidbod.detectCollisions = false;
 		StartCoroutine(setTimeOutDestroy());
 	}
-
-	void OnCollisionEnter (Collision collision) {
-		if (!Network.isServer){ return; }
-		Network.Destroy(gameObject);
-		if (collision.gameObject.name == "Player(Clone)"){
-				collision.gameObject.GetComponent<NetworkView> ().RPC("healthLoss", RPCMode.All, 5);
+	void FixedUpdate() {
+		gameObject.transform.rotation = Quaternion.LookRotation(rigidbod.velocity.normalized);
+		currentPosition = gameObject.transform.position;
+		positionDifference = (currentPosition - lastPosition).magnitude;
+		// check if there was a collision in the last 0.1 units
+		if (positionDifference > 0.1f) {
+			if (Physics.Raycast(lastPosition,rigidbod.velocity.normalized, out hit, positionDifference)) {
+				Network.Destroy(gameObject);
+				if (hit.collider.gameObject.name == "Player(Clone)") {
+					hit.collider.gameObject.GetComponent<NetworkView> ().RPC("healthLoss", RPCMode.All, 5);
+				}
+			}
+			lastPosition = currentPosition;
 		}
 	}
+	/*
+	void OnCollisionEnter (Collision collision) {
+		Network.Destroy(gameObject);
+		if (collision.gameObject.name == "Player(Clone)"){
+			bulletHit(collision.gameObject);
+		}
+	}*/
 
 	private IEnumerator setTimeOutDestroy () {
 		yield return new WaitForSeconds(LIFE_SPAN);
