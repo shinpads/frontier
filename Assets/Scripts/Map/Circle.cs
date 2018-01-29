7db0aca9e1c4987e378de351d2f425e8c6055c08
mootgoat <35712997+mootgoat@users.noreply.github.com>
2018-01-28 19:37:20 -0500

    wrote code to calculate where next circle origin should be and put playerGUI in the player subfolder

---
+++ Assets/Scripts/Map/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	Vector3 newOrigin(Vector3 origin, float oldRadius, float newRadius){
+		if (newRadius > oldRadius) { return; }
+		float tempRadius = Random.Range (0f, (oldRadius - newRadius));
+		float angle = Random.Range (0f, 2f * Mathf.PI);
+		origin.x = Mathf.Sqrt(tempRadius) * Mathf.Cos (angle);
+		origin.z = Mathf.Sqrt(tempRadius) * Mathf.Sin (angle);
+		return (origin);
+	}
+}
