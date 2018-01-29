f8494dfbcae9a0f70c86f15e416a931ea966d95d
mootgoat <35712997+mootgoat@users.noreply.github.com>
2018-01-28 20:57:09 -0500

    fixed += in circle class

--- Assets/Scripts/Map/Circle.cs
+++ Assets/Scripts/Map/Circle.cs
@@ -8,16 +8,12 @@ public class Circle : MonoBehaviour {
 	void Start () {
 	}
 
-	// Update is called once per frame
-	void Update () {
-	}
-
 	Vector3 newOrigin(Vector3 origin, float oldRadius, float newRadius){
 		if (newRadius > oldRadius) { return; }
 		float tempRadius = Random.Range (0f, (oldRadius - newRadius));
 		float angle = Random.Range (0f, 2f * Mathf.PI);
-		origin.x = Mathf.Sqrt(tempRadius) * Mathf.Cos (angle);
-		origin.z = Mathf.Sqrt(tempRadius) * Mathf.Sin (angle);
+		origin.x += Mathf.Sqrt(tempRadius) * Mathf.Cos (angle);
+		origin.z += Mathf.Sqrt(tempRadius) * Mathf.Sin (angle);
 		return (origin);
 	}
 }
