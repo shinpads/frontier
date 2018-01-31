81093538b9bce02e8a77df02c4a2fe6886cdd9b9
Neraj Manamperi <neraj.manamperi@mail.utoronto.ca>
2018-01-30 22:42:14 -0500

    implemented shooting delay, and changed bullet speed

--- Assets/Scripts/Player/Shooting.cs
+++ Assets/Scripts/Player/Shooting.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Shooting : MonoBehaviour {
 	Camera playerCamera;
 	float shootTime = 10f;
+    public const float SHOT_DELAY = 0.5f;
 	RaycastHit hit;
 	Ray ray;
 	Vector3 endpoint;
 	float distance;
+    bool canshoot = true;
 	[SerializeField] GameObject tipOfGun;
 	NetworkView networkview;
 	void Start () {
@@ -20,8 +22,9 @@ public class Shooting : MonoBehaviour {
 
 	void Update () {
 		if (!networkview.isMine) { return; }
-		if (Input.GetButtonDown ("Fire1")) {
-			//Get Point where bullet will hit
+		if (Input.GetButtonDown ("Fire1") && canshoot == true) {
+            //Get Point where bullet will hit
+            StartCoroutine(delayedShooting());
 			ray = new Ray(playerCamera.transform.position,playerCamera.transform.forward*100);
 			if (Physics.Raycast(ray ,out hit)) {
 				endpoint = ray.GetPoint(hit.distance);
@@ -38,6 +41,11 @@ public class Shooting : MonoBehaviour {
 		if(!Network.isServer) { return; }
 		//create the bullet at tip of gun
 		GameObject shot = (GameObject) Network.Instantiate ((GameObject)Resources.Load("Prefabs/Bullet"), start,Quaternion.LookRotation(Vector3.Normalize(end-start)), 0);
-		shot.GetComponent<Rigidbody>().velocity = Vector3.Normalize(end-start)*100;
+		shot.GetComponent<Rigidbody>().velocity = Vector3.Normalize(end-start)*300;
 	}
+    private IEnumerator delayedShooting(){
+        canshoot = false;
+        yield return new WaitForSeconds(SHOT_DELAY);
+        canshoot = true;
+    }
 }
