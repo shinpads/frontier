36822aa75b1bea30b0533af758b1ea97309b4eea
marshmelllo <robfarlow@gmail.com>
2018-03-02 19:02:17 -0500

    created GameController.cs

---
+++ Assets/Scripts/Control/GameController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	void Start () {
+
+	}
+	void Update () {
+
+	}
+}
