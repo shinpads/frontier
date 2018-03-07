d0c4c0edee8597cc2ed04b983de8c0a2afcc7f1f
marshmelllo <robfarlow@gmail.com>
2018-03-06 23:37:12 -0500

    The-If

--- Assets/Scripts/Helpers/Global.cs
+++ Assets/Scripts/Helpers/Global.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 
 public class Global {
-	// Player Indicies 
+	// Player Indicies
 	public const int CHARACTER_TANK = 0;
 	public const int CHARACTER_SCOUT = 1;
 	public const int CHARACTER_THIEF = 2;
 	public const int CHARACTER_OTHER = 3;
 	public const int CHARACTER_ASSUALT = 4;
-	public static string[] CHARACTER_NAMES = new string[] {"Tank", "Scout", "Theif", "Other", "Assualt"};
+	public static string[] CHARACTER_NAMES = new string[] {"Tank", "Scout", "Thief", "Other", "Assualt"};
 }
