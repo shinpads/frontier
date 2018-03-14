7db9eeabc0e045acd20bd7d27752d7d523c6bff0
mootgoat <35712997+mootgoat@users.noreply.github.com>
2018-03-14 19:36:44 -0400

    fixing not being able to shoot if one of your weapons runs out of ammo

--- Assets/Scripts/Player/Shooting.cs
+++ Assets/Scripts/Player/Shooting.cs
@@ -65,11 +65,7 @@ public class Shooting : MonoBehaviour {
 			swapGuns (semiAuto);
 		}
 
-		if (currentGun.getAmmo() == 0) {
-			canShoot = false;
-		}
-
-		if (Input.GetButtonDown ("Fire1") && canShoot && !isReloading) {
+		if (Input.GetButtonDown ("Fire1") && canShoot && !isReloading && currentGun.getAmmo() != 0) {
       		//Get Point where bullet will hit
       		StartCoroutine(delayedShooting());
      		armPivotAnimator.SetTrigger("shooting");
