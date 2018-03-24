3228ad0a3b77a09bd62f12ad4c1469346fa1c43b
marshmelllo <robfarlow@gmail.com>
2018-03-24 01:11:24 -0400

    fixed some dynamite

--- Assets/Scripts/Game Mechanics/Explosion.cs
+++ Assets/Scripts/Game Mechanics/Explosion.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour {
 	[HideInInspector] public int userId;
+	bool doDamage = true;
+	void Start() {
+		if (!PhotonNetwork.isMasterClient) { this.enabled = false; }
+		StartCoroutine(destroyObject());
+	}
 	void OnTriggerEnter(Collider col) {
+		if (!doDamage) { return; }
 		if (col.gameObject.tag == "Player") {
 			col.gameObject.GetComponent<PhotonView> ().RPC("setHealth", PhotonTargets.All, -50, userId);
 		}
+	}
+	private IEnumerator removeCollider() {
+		yield return new WaitForSeconds(0.05f);
+		doDamage = false;
+	}
+	private IEnumerator destroyObject () {
+			yield return new WaitForSeconds(5f);
+			PhotonNetwork.Destroy(gameObject);
 	}
 }
