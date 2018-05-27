5aa42144b16708398b1381529df45d9865a38b80
marshmelllo <robfarlow@gmail.com>
2018-05-27 15:26:08 -0400

    lock cursor on focous

--- Assets/Scripts/Control/GameController.cs
+++ Assets/Scripts/Control/GameController.cs
@@ -326,4 +326,10 @@ public class GameController : MonoBehaviour {
 		StartCoroutine(clearKillFeedEvents());
 	}
 	public int getThisTeam() { return thisTeam; }
+	void OnApplicationFocus(bool hasFocus) {
+			if (hasFocus) {
+				Screen.lockCursor = true;
+				Cursor.visible = false;
+			}
+	}
 }
