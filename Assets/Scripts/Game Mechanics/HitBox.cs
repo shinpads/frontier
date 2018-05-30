8f19bee7f6af83583776db2f8e025217fa2658f3
marshmelllo <robfarlow@gmail.com>
2018-05-30 00:58:22 -0400

    made it so you can shoot people

--- Assets/Scripts/Game Mechanics/HitBox.cs
+++ Assets/Scripts/Game Mechanics/HitBox.cs
@@ -6,11 +6,7 @@ public class HitBox : MonoBehaviour {
 	[SerializeField] private float damageMultiplier = 1.0f;
 	[SerializeField] private GameObject playerObject;
 	[SerializeField] private bool isHead;
-	void Start() {
-		if(playerObject.GetComponent<PhotonView>().isMine) {
-			gameObject.layer = 13;
-		}
-	}
+
 	public float getDamageMultipler() { return damageMultiplier; }
 	public GameObject getPlayerObject() { return playerObject; }
 	public bool getIsHead() { return isHead; }
