7fa91d4b687716c043b89c3f356f3fd0f1d3b157
marshmelllo <robfarlow@gmail.com>
2018-06-03 13:30:04 -0400

    added disconnect button

--- Assets/Scripts/Control/EscapeMenu.cs
+++ Assets/Scripts/Control/EscapeMenu.cs
@@ -1,28 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EscapeMenu : MonoBehaviour {
 	[SerializeField] GameObject escapeMenuCanvas;
 	private bool enabled = false;
 	private GameObject playerObject;
+	[Header("UI Elements")]
+	public Button buttonContinue;
+	public Button buttonDisconnect;
+	public Button buttonOptions;
+	void Start() {
+			buttonContinue.onClick.AddListener(returnFromMenu);
+			buttonDisconnect.onClick.AddListener(disconnect);
+	}
 	void Update() {
 		if (Input.GetKeyDown(KeyCode.Escape)) {
 			playerObject = gameObject.GetComponent<GameController>().getPlayerObject();
 			enabled = !enabled;
-			if (enabled) {
-				Screen.lockCursor = false;
-				Cursor.visible = true;
-				escapeMenuCanvas.SetActive(true);
-				playerObject.GetComponent<Shooting>().setFreeze(true);
-				playerObject.GetComponent<PlayerController>().setFreeze(true);
-			} else {
-				Screen.lockCursor = true;
-				Cursor.visible = false;
-				escapeMenuCanvas.SetActive(false);
-				playerObject.GetComponent<Shooting>().setFreeze(false);
-				playerObject.GetComponent<PlayerController>().setFreeze(false);
-			}
+			updateEnabled();
 		}
+	}
+	void updateEnabled() {
+		if (enabled) {
+			Screen.lockCursor = false;
+			Cursor.visible = true;
+			escapeMenuCanvas.SetActive(true);
+			playerObject.GetComponent<Shooting>().setFreeze(true);
+			playerObject.GetComponent<PlayerController>().setFreeze(true);
+		} else {
+			Screen.lockCursor = true;
+			Cursor.visible = false;
+			escapeMenuCanvas.SetActive(false);
+			playerObject.GetComponent<Shooting>().setFreeze(false);
+			playerObject.GetComponent<PlayerController>().setFreeze(false);
+		}
+	}
+	void returnFromMenu() {
+		enabled = false;
+		updateEnabled();
+	}
+	void disconnect() {
+		enabled = false;
+		PhotonNetwork.Disconnect();
 	}
 }
