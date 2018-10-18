d84b66538af9aafc7d5a7eefc1589e740cf50631
marshmelllo <robfarlow@gmail.com>
2018-10-17 21:08:31 -0400

    camera animation on main menu

--- Assets/Scripts/Control/MainMenu.cs
+++ Assets/Scripts/Control/MainMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class MainMenu : MonoBehaviour {
+	[SerializeField] GameObject camera;
 	[Header("Buttons")]
 	[SerializeField] Button buttonFindGame;
 	[SerializeField] Button buttonOptions;
@@ -13,23 +14,48 @@ public class MainMenu : MonoBehaviour {
 	[Header("Other Elements")]
 	[SerializeField] GameObject loadingSpinner;
 	[SerializeField] GameObject mainPanel;
+
+	// Camera movement
+	Vector3 cameraMenuPosition;
+	Vector3 cameraLoadingPosition;
+
+
 	void Start() {
+		// buttons
 		buttonFindGame.onClick.AddListener(findGame);
 		buttonOptions.onClick.AddListener(goToOptions);
 		buttonExit.onClick.AddListener(exitGame);
+
+		// camera position
+		cameraMenuPosition = camera.transform.position;
+		cameraLoadingPosition = cameraMenuPosition;
+		cameraLoadingPosition.x += 0.9f;
 	}
+
 	private void findGame() {
-		loadingSpinner.SetActive(true);
-		mainPanel.SetActive(false);
-		mainMenuSection.SetActive(false);
-		gameObject.GetComponent<NetworkManager>().JoinServer();
+		StartCoroutine(moveCamera(cameraMenuPosition, cameraLoadingPosition, 0.35f));
 	}
+
 	private void goToOptions() {
 		optionsSection.GetComponent<OptionsController>().loadOptions();
 		mainMenuSection.SetActive(false);
 	}
+
 	private void exitGame() {
 		// only works in builds
 		Application.Quit();
+	}
+
+	private IEnumerator moveCamera(Vector3 from, Vector3 to, float time) {
+		float startTime = Time.time;
+		float endTime = startTime + time;
+		while (Time.time < endTime) {
+			camera.transform.position = Vector3.Lerp(from, to, (Time.time - startTime) / time);
+			yield return new WaitForEndOfFrame();
+		}
+		loadingSpinner.SetActive(true);
+		mainPanel.SetActive(false);
+		mainMenuSection.SetActive(false);
+		gameObject.GetComponent<NetworkManager>().JoinServer();
 	}
 }
